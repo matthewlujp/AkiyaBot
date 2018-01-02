8a1332aca398b3528f3df9fc216fd999617f30bd
Matthew Ishige <matthewlujp@gmail.com>
2018-01-02 17:34:25 +0900

    Tweak

--- ssh-tunnel/main.go
+++ ssh-tunnel/main.go
@@ -22,10 +22,13 @@ type config struct {
 }
 
 var (
-	cnf           config
-	retryInterval = 5 * time.Second
-	logger        = log.New(os.Stdout, "", 0)
-	forwardPort   *int
+	cnf             config
+	retryInterval   = 5 * time.Second
+	logger          = log.New(os.Stdout, "", 0)
+	forwardPort     *int
+	interEndpoint   *endpoint
+	forwardEndpoint *endpoint
+	localEndpoint   *endpoint
 )
 
 func init() {
@@ -41,27 +44,80 @@ func init() {
 	logger.Printf("user: %s ", cnf.RelayUserName)
 	logger.Printf("port: %d", *forwardPort)
 	logger.Printf("private key: %s", cnf.PrivateKeyPath)
+
+	interEndpoint = &endpoint{ // Endpoint for reverse ssh
+		host: cnf.RelayHostName,
+		port: 22,
+	}
+	forwardEndpoint = &endpoint{
+		host: "localhost",
+		port: *forwardPort,
+	}
+	localEndpoint = &endpoint{
+		host: "localhost",
+		port: 22,
+	}
 }
 
 func handleClient(client, remote net.Conn) {
 	defer client.Close()
-	chDone := make(chan bool)
+	done := make(chan struct{})
 
 	go func() {
 		if _, err := io.Copy(client, remote); err != nil {
 			logger.Printf("error while copy remote -> local: %s", err)
 		}
-		chDone <- true
+		close(done)
 	}()
 
 	go func() {
 		if _, err := io.Copy(remote, client); err != nil {
 			logger.Printf("error while copy local -> remote: %s", err)
 		}
-		chDone <- true
+		close(done)
 	}()
 
-	<-chDone
+	<-done
+}
+
+func connect(sshConfig *ssh.ClientConfig, quit chan<- struct{}) {
+	serverConn, err := ssh.Dial("tcp", interEndpoint.String(), sshConfig)
+	if err != nil {
+		logger.Print(err)
+		close(quit)
+		return
+	}
+	logger.Print("server connected")
+
+	listener, err := serverConn.Listen("tcp", forwardEndpoint.String())
+	if err != nil {
+		logger.Print(err)
+		close(quit)
+		return
+	}
+	logger.Print("server listener created")
+	defer listener.Close()
+
+	for {
+		local, err := net.Dial("tcp", localEndpoint.String()) // local and forwared request are conneced
+		if err != nil {
+			logger.Print(err)
+			close(quit)
+			return
+		}
+		logger.Print("local connected")
+
+		client, err := listener.Accept()
+		if err != nil {
+			logger.Print(err)
+			close(quit)
+			return
+		}
+		logger.Print("client accepted")
+
+		handleClient(client, local)
+	}
+
 }
 
 func main() {
@@ -71,19 +127,6 @@ func main() {
 	}
 	log.Printf("private key file %s read successfully", cnf.PrivateKeyPath)
 
-	interEndpoint := &endpoint{ // Endpoint for reverse ssh
-		host: cnf.RelayHostName,
-		port: 22,
-	}
-	forwardEndpoint := &endpoint{
-		host: "localhost",
-		port: *forwardPort,
-	}
-	localEndpoint := &endpoint{
-		host: "localhost",
-		port: 22,
-	}
-
 	sshConfig := &ssh.ClientConfig{
 		User:            cnf.RelayUserName,
 		Auth:            []ssh.AuthMethod{auth},
@@ -92,48 +135,9 @@ func main() {
 
 	// Never dies
 	for {
-		errChan := make(chan error)
-
-		go func() {
-			serverConn, err := ssh.Dial("tcp", interEndpoint.String(), sshConfig)
-			if err != nil {
-				errChan <- err
-				logger.Print(err)
-				return
-			}
-			logger.Print("server connected")
-
-			listener, err := serverConn.Listen("tcp", forwardEndpoint.String())
-			if err != nil {
-				logger.Print(err)
-				errChan <- err
-				return
-			}
-			logger.Print("server listener created")
-			defer listener.Close()
-
-			for {
-				local, err := net.Dial("tcp", localEndpoint.String()) // local and forwared request are conneced
-				if err != nil {
-					logger.Print(err)
-					errChan <- err
-					return
-				}
-				logger.Print("local connected")
-
-				client, err := listener.Accept()
-				if err != nil {
-					logger.Print(err)
-					errChan <- err
-					return
-				}
-				logger.Print("client accepted")
-
-				handleClient(client, local)
-			}
-		}()
-
-		<-errChan
+		quit := make(chan struct{})
+		connect(sshConfig, quit)
+		<-quit
 		logger.Printf("retry connection after %v seconds", retryInterval)
 		time.Sleep(retryInterval)
 	}
