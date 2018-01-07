f3416657bd13fa770a83cee43e31ecf63e572fc9
Matthew Ishige <matthewlujp@gmail.com>
2018-01-07 11:46:29 +0900

    Tweak

--- bot/gdrive/api.go
+++ bot/gdrive/api.go
@@ -64,7 +64,7 @@ func (gService *APIService) Upload(upFile *UploadFileInfo) error {
 		return err
 	}
 
-	f, err = gService.Files.Create(&drive.File{Name: upFile.Title, Parents: []string{folder.Id}}).Media(upFile.File).Do()
+	f, err := gService.Files.Create(&drive.File{Name: upFile.Title, Parents: []string{folder.Id}}).Media(upFile.File).Do()
 	logger.Printf("uploaded %s", f.Name)
 	return err
 }
